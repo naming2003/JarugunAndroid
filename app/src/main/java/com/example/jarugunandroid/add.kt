b7cbccab9bae92ed8393f225160e5aecb3774d09
naming11 <khunings9237@gmail.com>
2024-09-02 23:11:39 +0700

    vs 0.211

---
+++ app/src/main/java/com/example/jarugunandroid/add.kt
@@ -0,0 +1,30 @@
+package com.example.jarugunandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class add : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button1 = findViewById<Button>(R.id.button1)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            intent = Intent(this, see::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
